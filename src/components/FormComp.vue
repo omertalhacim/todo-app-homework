4f8dadd77547a4c699197f9e8f8a5e23765669a3
OmerTalhaCim <omertalhacim@gmail.com>
2020-07-16 15:02:35 +0300

    validation added

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -1,46 +1,70 @@
 <template>
   <div>
-    <form @submit.prevent="submitOperation" class="card p-3 bg-secondary text-white">
-      <div class="form-group">
-        <label>Title</label>
-        <input
-          type="text"
-          v-model="formModel.title"
-          class="form-control text-dark"
-          placeholder="Please enter title"
-        />
-      </div>
-      <div class="form-group">
-        <label>Description</label>
-        <textarea
-          class="form-control text-dark"
-          v-model="formModel.description"
-          rows="3"
-          placeholder="Please enter description"
-        ></textarea>
-      </div>
-      <div class="form-group">
-        <label>Status</label>
-        <select class="form-control" v-model="formModel.status">
-          <option value hidden disabled selected>Please Select</option>
-          <option class="bg-danger text-white" value="waiting">Waiting</option>
-          <option class="bg-primary text-white" value="inProgress">In Progress</option>
-          <option class="bg-success text-white" value="completed">Completed</option>
-        </select>
-      </div>
-      <br />
-      <button
-        type="submit"
-        class="btn text-white"
-        :style="isUpdate ? 'background-color:red' : 'background-color:green'"
-      >{{isUpdate ? 'Update' : 'Add New'}}</button>
-    </form>
+    <ValidationObserver v-slot="{ invalid }">
+      <form @submit.prevent="submitOperation" class="card p-3 bg-secondary text-white">
+        <div class="form-group">
+          <label>Title</label>
+          <ValidationProvider rules="required" v-slot="{ errors }">
+            <input
+              type="text"
+              v-model="formModel.title"
+              class="form-control text-dark"
+              placeholder="Please enter title"
+            />
+            <span style="color: red">{{ errors[0] }}</span>
+          </ValidationProvider>
+        </div>
+        <div class="form-group">
+          <label>Description</label>
+          <ValidationProvider rules="required" v-slot="{ errors }">
+            <textarea
+              class="form-control text-dark"
+              v-model="formModel.description"
+              rows="3"
+              placeholder="Please enter description"
+            ></textarea>
+            <span style="color: red">{{ errors[0] }}</span>
+          </ValidationProvider>
+        </div>
+        <div class="form-group">
+          <label>Status</label>
+          <ValidationProvider rules="required" v-slot="{ errors }">
+            <select class="form-control" v-model="formModel.status">
+              <option value hidden disabled selected>Please Select</option>
+              <option class="bg-danger text-white" value="waiting">Waiting</option>
+              <option class="bg-primary text-white" value="inProgress">In Progress</option>
+              <option class="bg-success text-white" value="completed">Completed</option>
+            </select>
+            <span style="color: red">{{ errors[0] }}</span>
+          </ValidationProvider>
+        </div>
+        <br />
+        <button
+          :disabled="invalid"
+          type="submit"
+          class="btn text-white"
+          :style="isUpdate ? 'background-color:red' : 'background-color:green'"
+        >{{isUpdate ? 'Update' : 'Add New'}}</button>
+      </form>
+    </ValidationObserver>
   </div>
 </template>
 
 <script>
+import { ValidationProvider, extend, ValidationObserver } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+
+extend("required", {
+  ...required,
+  message: "*This field is required"
+});
+
 export default {
   name: "FormComp",
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   props: {
     isUpdate: {
       type: Boolean
